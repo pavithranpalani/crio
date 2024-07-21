7d2c5a8ae5c4f67529878e92163d5253b5a9bd5c
PAVITHRAN <pavithran95palani@gmail.com>
2024-07-21 19:39:23 +0530

    completed

---
+++ stopwatch/stopwatch/src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const Stopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const startStopwatch = () => {
+    setIsRunning(true);
+  };
+
+  const stopStopwatch = () => {
+    setIsRunning(false);
+  };
+
+  const resetStopwatch = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(1, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  };
+
+  return (
+    <div className="stopwatch">
+      <h2>Stopwatch</h2>
+      <div className="time">Time: {formatTime()}</div>
+
+      <div className="buttons">
+        {isRunning ? (
+          <button onClick={stopStopwatch}>Stop</button>
+        ) : (
+          <button onClick={startStopwatch}>Start</button>
+        )}
+        <button onClick={resetStopwatch}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
